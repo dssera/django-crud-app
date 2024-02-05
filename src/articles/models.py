b128f281f39b3b759a4fbd254ef501db3bbda732
dssera <adesser001@mail.ru>
2024-02-05 17:26:55 +0300

    added and registered Article model

--- src/articles/models.py
+++ src/articles/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+class Article(models.Model):
+
+    class Status(models.TextChoices):
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB', 'Published'
+
+    title = models.CharField(max_length=50, 
+                             blank=False)
+    body = models.TextField(blank=False)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=3,
+                              choices=Status.choices,
+                              default=Status.DRAFT)
+    
+    class Meta:
+        ordering = ['-publish']
+
+    def __str__(self):
+        return self.title
