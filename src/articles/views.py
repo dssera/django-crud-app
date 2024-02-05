4ff753bf007eddf1ab4d6717f1627fe903ef9801
dssera <adesser001@mail.ru>
2024-02-05 22:10:45 +0300

    added filter for articles

--- src/articles/views.py
+++ src/articles/views.py
@@ -1,7 +1,24 @@
+from functools import reduce
+import operator
+from typing import Any
+
+from django.db.models.query import QuerySet
 from django.shortcuts import render
 from django.views import generic
+from django.db.models import Q
 
 from .models import Article
 
 class ArticleListView(generic.ListView):
     model = Article
+
+    def get_queryset(self) -> QuerySet[Any]:
+        q = self.request.GET.get('q')
+
+        # PB status means 'published'
+        object_list = self.model.objects.filter(status='PB')
+        if q:
+            q_list = [Q(body__icontains=q), Q(author__username__icontains=q)]
+            object_list = object_list.filter(reduce(operator.or_, q_list))
+        return object_list
+    
